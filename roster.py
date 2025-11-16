4efdee1bbf2ebd98d491b1e1d52a2256e34f7746
shriyaagarwal1 <shriyaa20x5@gmail.com>
2025-11-16 15:35:07 -0500

    Final

--- roster.py
+++ roster.py
@@ -1,12 +1,11 @@
-# https://goheels.com/sports/mens-basketball/roster
-#roster = ['Bacot', 'Davis', 'Cadeau']
-#for player in roster:
-    #print(player)
-    
 import pandas as pd
-player = {"Last Name": ["Bacot", "Davis", "Cadeau"],
-          "First Name": ["Armando", "RJ", "Elliot"],
-          "height": [83, 72, 73],
-          "weight": [240, 180, 180]}
+player = {"Last Name": ["Bacot", "Davis", "Cadeau", "Evans", "High", "Brown", "Dixon", "Young", "Denis", "Davis"],
+          "First Name": ["Armando", "RJ", "Elliot", "Kyan", "Zayden", "James","Derek","Jaydon", "Isaiah", "Elijah"],
+          "height": [83, 72, 73, 72.24, 73.2, 73.2, 72.6, 72.48, 72.48, 72.36],
+          "weight": [240, 180, 180, 175, 230, 240, 200, 200, 180, 205]}
 data =pd.DataFrame(player)
+data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+data["bmi"] = data["bmi"].round(2)
 print(data)
+data.to_csv("bmi.csv")
+
